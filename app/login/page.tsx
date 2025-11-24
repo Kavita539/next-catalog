a0864b3079db23c6be9d52f3dbb4730db690aaab
Kavita Pathak <pathakkavita273@gmail.com>
2025-11-24 16:50:29 +0530

    feat: Create Login Page

---
+++ app/login/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import styles from "@/styles/Login.module.css";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => setError(""), 3000);
+      return () => clearTimeout(timer);
+    }
+  },[error]);
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (email && password) {
+      router.push("/");
+    } else {
+      setError("Please enter both email and password.");
+      return;
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardWrapper}>
+        <div className={styles.card}>
+          <div className={styles.signInHeader}>Sign in</div>
+          <form className={styles.form} onSubmit={handleLogin}>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email Address*"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className={styles.inputField}
+            />
+
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password*"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className={styles.inputField}
+            />
+
+            {error && <div className={styles.error}>{error}</div>}
+
+            <button type="submit" className={styles.signInButton}>
+              Sign in
+            </button>
+
+            <div className={styles.linkGroup}>
+              <a href="#" className={styles.link}>
+                Forgot password?
+              </a>
+              <a href="#" className={styles.link}>
+                Don't have an account? Sign Up
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
